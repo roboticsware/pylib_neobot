a9b017f45a0026ca17af71b8e018385f62f47427
BCKM <108933157+BCKM@users.noreply.github.com>
2022-08-10 12:20:41 +0500

    led_off('out1')

--- test.py
+++ test.py
@@ -11,11 +11,11 @@ n =  Neosoco()
 # wait(1000)
 
 # case3) Turn on LED when the distance is under 10cm
-# while True:
-#   if n.get_value('in1') < 10:
-#     n.led_on('out1', '100')
-#   else:
-#     n.set_value('out1', 0)
+while True:
+  if n.get_value('in1') < 10:
+    n.led_on('out1', '100')
+  else:
+    n.led_off('out1')
 
 # case4) Move forth and back during 1s and stop
 # n.motor_move('forward')
